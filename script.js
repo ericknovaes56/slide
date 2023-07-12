588459477c88c702de9aa5e21d87d4ede435cd8e
3nderXP <adrian08cavalcante09santos@gmail.com>
2023-07-12 08:01:29 -0300

    Bazinga

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-const debounce = (c, w = 1000, t = null) => {
-
-    try {
-
-        return (...args) => {
-
-            clearTimeout(t)
-            t = setTimeout(() => {
-
-                c(...args)
-
-            }, w)
-
-        }
-
-    } catch (Error) {
-
-        console.error(Error.message)
-
-    }
-
-}
-
 const clients = [
     {
         name: 'Gisele Meireles',
         description: 'Descrição da Gisele Meireles',
         photo: 'https://cdn.discordapp.com/attachments/801210889943384105/1128464520185131168/logo.png',
-        projects: [
+        images: [
             {
-                image: 'https://images-ext-1.discordapp.net/external/4ruXRu7zm5rKR538X1ANskR5J41ZihVJGxGYDbjDmAs/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/369230482237423617/2abaa661c51bfbd4d4466b6918f78d1e.png?size=2048',
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_338756439_602068328649375_970361855913615907_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_339145169_1269735400298651_4063940089860892140_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342039584_756499279431425_3093297704798233902_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342065312_698465782053602_7578099195561972987_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342067728_749248463361735_8298239884568301860_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342083275_1411824366221165_5357967678716503921_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342101572_768162264720561_3240492123577296049_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342280851_231521492885132_5469276481783619988_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342373556_888794072225970_6394532793165796485_n_1080 (1).jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/gisela_meireles/images/Snapinsta.app_342377721_1322063631704741_5339397734611834695_n_1080 (1).jpg',
                 page: '#'
             },
         ]
@@ -37,9 +50,45 @@ const clients = [
         name: 'Matheus Staudinger',
         description: 'Descrição do Matheus Staudinger',
         photo: 'https://cdn.discordapp.com/attachments/801210889943384105/1128464553248825424/logo.png',
-        projects: [
+        images: [
             {
-                image: 'https://images-ext-1.discordapp.net/external/4ruXRu7zm5rKR538X1ANskR5J41ZihVJGxGYDbjDmAs/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/369230482237423617/2abaa661c51bfbd4d4466b6918f78d1e.png?size=2048',
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/m1.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348488684_1034266564203321_7536135470659574564_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348647709_1610231812834414_5296188404553254524_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348654148_205637658971834_6024116929798325022_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348662650_127263963697782_6036216806623176685_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348664033_1380204189487913_7907616534426737778_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348670622_937757114157421_4157285723538284629_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348749239_276612601469460_905745581815246018_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_348862042_1804157626646932_7698996856201145835_n_1080.jpg',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/matheus_staudinger/images/Snapinsta.app_349040524_989746442016645_1782292294069088091_n_1080.jpg',
                 page: '#'
             },
         ]
@@ -48,9 +97,45 @@ const clients = [
         name: 'Pure Healthy',
         description: 'Descrição da Pure Healthy',
         photo: 'https://cdn.discordapp.com/attachments/801210889943384105/1128464577957470208/logo.png',
-        projects: [
+        images: [
             {
-                image: 'https://images-ext-1.discordapp.net/external/4ruXRu7zm5rKR538X1ANskR5J41ZihVJGxGYDbjDmAs/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/369230482237423617/2abaa661c51bfbd4d4466b6918f78d1e.png?size=2048',
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p1.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p2.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p3.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p4.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p5.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p6.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p7.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p8.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p9.png',
+                page: '#'
+            },
+            {
+                image: 'http://localhost/Gabriel%20other%20project/imgs/pure_healthy/images/p10.png',
                 page: '#'
             },
         ]
@@ -72,16 +157,73 @@ const elements = {
             description: document.querySelector('.slide .content .infos-top .client-infos .description')
         },
         btnAction: document.querySelector('.slide .content .infos-top .btn-action')
+    },
+    slideContainer: {
+        self: document.querySelector('.slide .content .slide-container'),
+        actions: {
+            self: document.querySelector('.slide .content .slide-container .actions'),
+            childs: document.querySelectorAll('.slide .content .slide-container .actions .action'),
+        },
+        images: {
+            self: document.querySelector('.slide .content .slide-container .images .group'),
+            childs: document.querySelectorAll('.slide .content .slide-container .images .group .image'),
+        }
     }
 }
 
 window.addEventListener('load', async () => {
+
+    const carouselResizeObserver = new ResizeObserver(async (entries) => {
+
+        for (entry of entries){
+
+            const currentImage = Array.from(elements.slideContainer.images.childs).filter((child) => {
+
+                return child.classList.contains('current')
+                
+            })[0]
+            const currentImageIndex = Array.from(elements.slideContainer.images.childs).indexOf(currentImage)
+
+            if(currentImageIndex > 0){
+
+                await scrollCarousel(elements.slideContainer.images.childs[currentImageIndex - 1], 'right')
+
+            }
+
+        }
+
+    })
+
+    carouselResizeObserver.observe(elements.slideContainer.self)
 
     const client = await getClient()
 
     elements.infosTop.foto.src = client.photo
     elements.infosTop.clientInfos.name.textContent = client.name
     elements.infosTop.clientInfos.description.textContent = client.description
+
+    if(client.images.length > 0){
+
+        client.images.forEach(async (project, index) => {
+
+            const projectContainer = document.createElement('a')
+            const projectImage = document.createElement('img')
+            
+            projectContainer.href = project.page
+            projectContainer.classList.add('image')
+
+            projectImage.src = project.image
+
+            projectContainer.appendChild(projectImage)
+            elements.slideContainer.images.self.appendChild(projectContainer)
+
+        })
+
+        elements.slideContainer.images.childs = elements.slideContainer.images.self.childNodes
+
+        await scrollCarousel(elements.slideContainer.images.childs[0], 'right')
+
+    }
 
     elements.infosTop.btnAction.addEventListener('click', async () => {
 
@@ -90,10 +232,92 @@ window.addEventListener('load', async () => {
         elements.infosTop.foto.src = client.photo
         elements.infosTop.clientInfos.name.textContent = client.name
         elements.infosTop.clientInfos.description.textContent = client.description
+
+        if(client.images.length > 0){
+
+            elements.slideContainer.images.self.innerHTML = null;
+
+            client.images.forEach(async (project, index) => {
+    
+                const projectContainer = document.createElement('a')
+                const projectImage = document.createElement('img')
+                
+                projectContainer.href = project.page
+                projectContainer.classList.add('image')
+    
+                projectImage.src = project.image
+    
+                projectContainer.appendChild(projectImage)
+                elements.slideContainer.images.self.appendChild(projectContainer)
+    
+            })
+
+            elements.slideContainer.images.childs = elements.slideContainer.images.self.childNodes
+
+            await scrollCarousel(elements.slideContainer.images.childs[0], 'right')
+    
+        }
     
     })
 
+    elements.slideContainer.actions.childs.forEach((action) => {
+
+        action.addEventListener('click', async () => {
+            
+            let currentImage = Array.from(elements.slideContainer.images.childs).filter((image) => {
+    
+                return image.classList.contains('current')
+    
+            })[0]
+
+            const currentImageIndex = Array.from(elements.slideContainer.images.childs).indexOf(currentImage)
+            const direction = action.dataset.direction
+
+            if((currentImageIndex == 0 && direction == 'left') || (currentImageIndex == elements.slideContainer.images.childs.length - 1 && direction == 'right')){
+
+                return
+
+            }
+            
+            await scrollCarousel(currentImage, action.dataset.direction)
+
+        })
+
+    })
+
 })
+
+async function scrollCarousel(currentImage, direction) {
+
+    elements.slideContainer.images.childs.forEach((image) => {
+
+        image.classList.remove('hide')
+        image.classList.remove('current')
+
+    })
+
+    const newCurrentImage = currentImage[`${direction == 'right' ? 'next' : 'previous'}Sibling`]
+    
+    newCurrentImage.classList.add('current')
+
+    if(newCurrentImage.previousSibling && newCurrentImage.previousSibling.previousSibling){
+
+        newCurrentImage.previousSibling.previousSibling.classList.add('hide')
+        
+    }
+
+    if(newCurrentImage.nextSibling && newCurrentImage.nextSibling.nextSibling){
+
+        newCurrentImage.nextSibling.nextSibling.classList.add('hide')
+
+    }
+
+    const newcurrentImageIndex = Array.from(elements.slideContainer.images.childs).indexOf(newCurrentImage) - 1
+    const offset = (newCurrentImage.clientWidth * -newcurrentImageIndex) + 'px'
+
+    elements.slideContainer.images.self.style.transform = `translateX(${offset})`
+
+}
 
 async function getClient() {
 
